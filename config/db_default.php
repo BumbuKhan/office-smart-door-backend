29b820181e9bd920a8542a6c44493230ebe4417d
BumbuKhan <qurban.qurbanov93@gmail.com>
2018-01-25 05:10:11 +0400

    db.php has been .gitignor-ed

--- config/db_default.php
+++ config/db_default.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=office-smart-door',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'root', // 'smart-door-user'
+    'password' => '', // 'mafu#hrF9shA'
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
